4b913b7723d0ce901d719bf5ca6cd9a0e1a31715
Peter Crall <pcrall04@gmail.com>
2020-06-16 18:10:22 -0700

    Made TimeRunnable an Interface

--- PeterTimer/src/peterTimer/TimeRunnable.java
+++ PeterTimer/src/peterTimer/TimeRunnable.java
@@ -2,9 +2,6 @@ package peterTimer;
 
 import org.bukkit.Bukkit;
 
-public class TimeRunnable {
-	
-	public void run(Timer timer) {
-		Bukkit.getConsoleSender().sendMessage("You Forgot To Setup Your Runnable!!!!!!!");
-	}
+interface TimeRunnable {
+	public void run(Timer timer);
 }
