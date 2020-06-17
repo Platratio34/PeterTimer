5d2e8d8f0b015aaf87ee2af193e89839799ed709
Peter Crall <pcrall04@gmail.com>
2020-06-16 18:28:58 -0700

    Removed testing timer from Main
    Left it in as a comment as an example

--- PeterTimer/src/peterTimer/Main.java
+++ PeterTimer/src/peterTimer/Main.java
@@ -7,21 +7,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
-	private Timer testTimer;
+//	private Timer testTimer;
 	
 	@Override
 	public void onEnable() {
-		TimeRunnable done = new TimeRunnable() {
-			@Override
-			public void run(Timer timer) {
-				Bukkit.getConsoleSender().sendMessage("Timer Done");
-			}
-		};
-		HashMap<Integer,TimeRunnable> map = new HashMap<Integer,TimeRunnable>();
-		map.put(0,done);
-		Bukkit.getConsoleSender().sendMessage("PeterTimer Started");
-		testTimer = new Timer(5*20, "testTimer", map, true, this);
-		testTimer.start();
+//		TimeRunnable done = new TimeRunnable() {
+//			public void run(Timer timer) {
+//				Bukkit.getConsoleSender().sendMessage("Timer Done");
+//			}
+//		};
+//		HashMap<Integer,TimeRunnable> map = new HashMap<Integer,TimeRunnable>();
+//		map.put(0,done);
+//		Bukkit.getConsoleSender().sendMessage("PeterTimer Started");
+//		testTimer = new Timer(5*20, "testTimer", map, true, this);
+//		testTimer.start();
 	}
 	
 	@Override
